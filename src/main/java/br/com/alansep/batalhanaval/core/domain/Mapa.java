f5c9c5fe7e74f752108fdfa76f9ec31f91fa5c7f
Gabriel Alan <alang.von@gmail.com>
2020-06-28 19:16:40 -0300

    Dependência de comunicação adicionada.

--- src/main/java/br/com/alansep/batalhanaval/core/domain/Mapa.java
+++
@@ -1,9 +0,0 @@
-package br.com.alansep.batalhanaval.core.domain;
-
-import br.com.alansep.batalhanaval.core.pattern.MapaService;
-
-public class Mapa implements MapaService {
-
-	private Posicao[][] posicoes = new Posicao[10][10];
-	
-}
